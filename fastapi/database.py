5494c6f0532943c7758fa30863404cac2f1e0d67
Thitiphan Tangsamphan <129848152+lktt@users.noreply.github.com>
2024-09-24 15:54:08 +0700

    commit Week 12 Lab

--- fastapi/database.py
+++ fastapi/database.py
@@ -33,6 +33,17 @@ async def get_user(user_id: int):
     query = "SELECT * FROM users WHERE user_id = :user_id"
     return await database.fetch_one(query=query, values={"user_id": user_id})
 
+# Function to select a user by user_id from the users table
+async def get_user(username: str):
+   query = "SELECT * FROM users WHERE username = :username"
+   return await database.fetch_one(query=query, values={"username": username})
+
+
+# Function to select a user by email from the users table
+async def get_user_by_email(email: str,password_hash:str):
+   query = "SELECT * FROM users WHERE email = :email and password_hash = :password_hash"
+   return await database.fetch_one(query=query, values={"email": email,"password_hash": password_hash})
+
 # Function to update a user in the users table
 async def update_user(user_id: int, username: str, password_hash: str, email: str):
     query = """
